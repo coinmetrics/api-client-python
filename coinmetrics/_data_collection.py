f4b8735d46ba052c7a4a9f58636d275798af1cb3
Victor Ramirez <vic@coinmetrics.io>
2021-09-27 15:52:33 -0500

    remove extra imports

--- coinmetrics/_data_collection.py
+++ coinmetrics/_data_collection.py
@@ -4,8 +4,6 @@ from io import BytesIO
 from logging import getLogger
 from typing import Any, Dict, Iterable, Iterator, List, Optional, cast
 
-import pandas
-import pandas as pd
 from pandas import DataFrame
 
 from coinmetrics._typing import DataRetrievalFuncType, FilePathOrBuffer, UrlParamTypes
@@ -79,7 +77,7 @@ class DataCollection:
             self._get_csv_data_lines(columns_to_store), path_or_bufstr, compress
         )
 
-    def to_dataframe(self, header: Optional[List[str]] = None) -> pandas.DataFrame:
+    def to_dataframe(self, header: Optional[List[str]] = None) -> DataFrame:
         data_generator = self._get_csv_data_lines(header)
         columns = next(data_generator).decode("utf-8").strip().split(",")
 
@@ -87,7 +85,7 @@ class DataCollection:
         for row_byte in data_generator:
             row_data = row_byte.decode("utf-8").strip().split(",")
             rows.append(row_data)
-        return pd.DataFrame(rows, columns=columns)
+        return DataFrame(rows, columns=columns)
 
     def _get_csv_data_lines(
         self, columns_to_store: Optional[List[str]]
