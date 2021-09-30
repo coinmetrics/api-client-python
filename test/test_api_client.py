8787b6cf50adfb823579f73ca4ffc0a156d5c4f9
Victor Ramirez <vic@coinmetrics.io>
2021-09-30 09:01:53 -0500

    create test functions without calling client

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -26,16 +26,27 @@ def test_base_url() -> None:
     assert CoinMetricsClient()._api_key_url_str == ""
 
 def test_to_dataframe() -> None:
-    client = CoinMetricsClient()
+    test_param_dict = {'asset': 'btc', 'metric': '1'}
+    test_data_dict = {'data': [test_param_dict for i in range(2)]}
+    test_data_iter = iter([test_data_dict])
+    test_header = ['col1', 'col2']
 
-    df_blk_test = client.get_asset_metrics(
-        assets='btc',
-        metrics=['BlkCnt'],
-        start_time='2019-10-19T00:00:00Z',
-        end_time='2019-10-19T00:00:05Z',
-        limit_per_asset=1
-    ).to_dataframe()
+    def test_data_retrieval_function(p1, p2): return test_data_dict
 
-    assert (df_blk_test['asset'] == 'btc').all()
-    assert 'BlkCnt' in df_blk_test.columns
-    assert df_blk_test.shape[0] == 1
+    test_data_collection = DataCollection(
+        data_retrieval_function=test_data_retrieval_function,
+        endpoint="",
+        url_params=test_param_dict
+    )
+    test_df = test_data_collection.to_dataframe()
+    assert test_df.shape == (2, 2)
+    assert list(test_param_dict.keys()) == list(test_df.columns)
+    assert (test_df['asset'] == 'btc').all()
+
+    test_data_collection_header = DataCollection(
+        data_retrieval_function=test_data_retrieval_function,
+        endpoint="",
+        url_params=test_param_dict
+    )
+    test_df_header = test_data_collection_header.to_dataframe(header=test_header)
+    assert list(test_df_header.columns) == test_header
