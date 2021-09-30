033208538a6409e6d9927725b5c18e2771b08c25
Victor Ramirez <vic@coinmetrics.io>
2021-09-30 09:33:02 -0500

    reformatting

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -25,28 +25,29 @@ def test_base_url() -> None:
     )
     assert CoinMetricsClient()._api_key_url_str == ""
 
+
 def test_to_dataframe() -> None:
-    test_param_dict = {'asset': 'btc', 'metric': '1'}
-    test_data_dict = {'data': [test_param_dict for i in range(2)]}
+    test_param_dict = {"asset": "btc", "metric": "1"}
+    test_data_dict = {"data": [test_param_dict for i in range(2)]}
     test_data_iter = iter([test_data_dict])
-    test_header = ['col1', 'col2']
+    test_header = ["col1", "col2"]
 
     test_data_retrieval_function = lambda x, y: test_data_dict
 
     test_data_collection = DataCollection(
         data_retrieval_function=test_data_retrieval_function,
         endpoint="",
-        url_params=test_param_dict
+        url_params=test_param_dict,
     )
     test_df = test_data_collection.to_dataframe()
     assert test_df.shape == (2, 2)
     assert list(test_param_dict.keys()) == list(test_df.columns)
-    assert (test_df['asset'] == 'btc').all()
+    assert (test_df["asset"] == "btc").all()
 
     test_data_collection_header = DataCollection(
         data_retrieval_function=test_data_retrieval_function,
         endpoint="",
-        url_params=test_param_dict
+        url_params=test_param_dict,
     )
     test_df_header = test_data_collection_header.to_dataframe(header=test_header)
     assert list(test_df_header.columns) == test_header
