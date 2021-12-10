1a2f06ab1b6e04bdd9d1a7f04d7ed8bd7bd2ff53
Victor Ramirez <vic@coinmetrics.io>
2021-12-10 15:21:25 +0000

    Resolve "Add bash script for updating version"

---
+++ update_version.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+VERSION=$1
+new_line_py="__version__ = \"$VERSION\""
+new_line_toml="version = \"$VERSION\""
+init=coinmetrics/__init__.py
+pyproject=pyproject.toml
+cat $init | while read line; do
+
+  if [[ $line =~ "__version__ = " ]]; then sed -i '' -e "s/$line/$new_line_py/" $init
+  fi
+
+done < $init
+cat $pyproject | while read line; do
+  if [[ $line =~ "version = " ]]; then sed -i '' -e "s/$line/$new_line_toml/" $pyproject
+  fi
+
+done < $pyproject
