b2d347cd9667a7ae53819007309900065eaa83c9
Alex Buchkovsky <oleksandr@coinmetrics.io>
2022-09-02 01:47:21 +0300

    add example for streaming order books

---
+++ examples/websockets/timeseries_stream_market_orderbooks.py
@@ -0,0 +1,35 @@
+import sys
+from os import environ
+
+import orjson
+
+from coinmetrics.api_client import CoinMetricsClient, CmStream
+
+api_key = (
+        environ.get("CM_API_KEY") or sys.argv[1]
+)  # sys.argv[1] is executed only if CM_API_KEY is not found
+
+client = CoinMetricsClient(api_key)
+stream = client.get_stream_market_orderbooks(
+    markets=['binance-btc-usdt-spot']
+)
+
+def on_message(
+        stream: CmStream, message: str
+) -> None:
+    """
+    Custom message callable to be passed in the streaming object
+    :param stream: CmStream, WebSocketApp connection
+    :param message: str, The message relayed by the API
+    :return: None
+    """
+    data = orjson.loads(message)
+    print(data)
+    sequence_id = int(data['cm_sequence_id'])
+    max_cm_sequence_id = 100
+    if sequence_id >= max_cm_sequence_id:
+        print(f"Closing the connection after {max_cm_sequence_id} messages...")
+        stream.close()
+
+# blocks until connection is closed or interrupted
+stream.run(on_message=on_message)
